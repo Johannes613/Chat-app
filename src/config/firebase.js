891c1b48717e7014943c989616956867b617ec24
Yohannis Adamu <160431822+Johannes613@users.noreply.github.com>
2025-04-25 01:38:45 +0400

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,15 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC9HqrHxRIJ8Lvddx413T5R_sBjaS4-870",
-  authDomain: "chat-app-8928c.firebaseapp.com",
-  projectId: "chat-app-8928c",
-  storageBucket: "chat-app-8928c.firebasestorage.app",
-  messagingSenderId: "585896329329",
-  appId: "1:585896329329:web:584ff9c5d3def1f8e29bd0",
-  measurementId: "G-CWYQ3GYTV0",
-};
+const firebaseConfig = {};
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
